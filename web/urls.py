f78dab2ce24158bc40a973c83b8d7cbc5352ee0a
Cheer.L <1017801883@qq.com>
2017-02-25 14:30:01 +0800

    首页可访问

--- web/urls.py
+++ web/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import view
 
 urlpatterns = [
+    url(r'^$', view.hello),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
 ]
+
