bbce92dab93e574db1aa3e3b0227076ee14cdc50
Cheer.L <1017801883@qq.com>
2017-02-25 21:54:06 +0800

    试试看登陆微信

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^button$', views.button),
+    url(r'^login$', views.login),
 ]
--- app/views.py
+++ app/views.py
@@ -5,25 +5,36 @@ from PIL import Image
 import itchat
 
 
-pic_name = 'QR.jpg'
+pic_name = 'pic.jpg'
+QR_name = 'QR.jpg'
 
 # Create your views here.
 def index(request):
     #pic = Photo()
-    text = '微信小助手'
+    if os.path.isfile('static' + pic_name):
+        text = '请扫码登陆'
+    else:
+        text = None
     result = {
-        'title':text,
-        'body':text,
-        'pic':pic_name
+        'title':'微信小助手',
+        'body':text or '微信小助手',
+        'pic':pic_name,
+        'QR':QR_name,
     }
     return render(request, 'app/index.html', result)
 
-def button(request):
+def login(request):
     itchat.auto_login(picDir='static' + pic_name, hotReload=True)
     friend = itchat.search_mps(name='微信支付')[0]
     itchat.send('2333', friend['UserName'])
-    itchat.run()
-    itchat.send('2333', friend['UserName'])
+    draw()
     #页面重定向
     return HttpResponseRedirect('.')
+
+def draw():
+    from random import randint
+    width = 100
+    height = 100
+    image = Image.new('RGB', (width, height), (randint(0, 255), randint(0, 255), randint(0, 255)))
+    image.save('static/' + pic_name, 'jpeg')
     
