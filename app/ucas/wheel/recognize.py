8ac9a2bf3c270773a609cbd474d8cfef5dad2671
Cheer.L <1017801883@qq.com>
2017-02-27 20:52:54 +0800

    req

--- app/ucas/wheel/recognize.py
+++ app/ucas/wheel/recognize.py
@@ -5,7 +5,7 @@ import sys
 import speech_recognition as sr
 from pydub import AudioSegment
 
-def spech_recognize(path):
+def spech_recognize(path, mode=0):
     '使用Bing进行语音识别, 可以自动转换mp3'
     if not path:
         return
@@ -23,7 +23,12 @@ def spech_recognize(path):
         audio = recognizer.record(source)
 
     try:
-        return recognizer.recognize_bing(audio, key, language='zh-cn')
+        if mode is 0:
+            return recognizer.recognize_bing(audio, key, language='zh-cn')
+        elif mode is '1':
+            return recognizer.recognize_google(audio, language='zh-cn')
+        else:
+            return
     except LookupError:
         return "我没有听懂"
     finally:
@@ -35,7 +40,11 @@ def main():
     path = ''
     if len(sys.argv) > 1:
         path = sys.argv[1]
-    print(spech_recognize(path))
+    if len(sys.argv) > 2:
+        text = spech_recognize(path, sys.argv[2])
+    else:
+        text = spech_recognize(path)
+    print(text)
 
 if __name__ == '__main__':
     main()
