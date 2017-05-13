655400e6671d0be05b119c95be071b963284d833
Cheer.L <1017801883@qq.com>
2017-05-13 15:23:57 +0800

    update

--- web/view.py
+++ web/view.py
@@ -1,6 +1,13 @@
 '默认初始页面视图'
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def hello(request):
-    'say: Hello world!'
-    return HttpResponse("Hello world!")
+    url_list = [
+        {'text': '小助手', 'url': 'helper'},
+        {'text': '博客', 'url': 'blog'},
+        {'text': '文件管理', 'url': 'file'},
+        {'text': 'VNC', 'url': 'vnc'}
+    ]
+    return render(request, 'index.html', dict(url_list=url_list))
