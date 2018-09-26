f3f4b35c7535446fc443e93341607e19e69fe83e
Cheer.L <1017801883@qq.com>
2018-09-27 01:39:51 +0800

    merge

--- ssh/urls.py
+++ ssh/urls.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^$', views.login),
     url(r'^logout/', views.logout),
-=======
-from django.conf.urls import url, include
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.login),
-    url(r'^logout/', views.logout),
->>>>>>> 5143386867406b24d8fcdf3d540e4c7d00de1472
 ] 
