72579c5a6bc64f1068ee557d337bbadb953827e8
Cheer.L <1017801883@qq.com>
2018-10-11 12:54:48 +0800

    sync

--- web/view.py
+++ web/view.py
@@ -8,6 +8,8 @@ def hello(request):
         {'text': '小助手', 'url': 'helper'},
         {'text': '文件管理', 'url': 'file'},
         {'text': 'VNC', 'url': 'vnc'},
-        {'text': 'Admin', 'url': 'admin'}
+        {'text': 'Admin', 'url': 'admin'},
+        {'text': '路由', 'url': 'cgi-bin/luci'},
+        {'text': 'Syncthing', 'url': 'syncthing'}
     ]
     return render(request, 'index.html', dict(url_list=url_list))
